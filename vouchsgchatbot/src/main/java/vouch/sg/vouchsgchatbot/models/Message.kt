4d7dfa0b925269d154f831d347a8987592e706e5
achmad abrar <achm.abrar@gmail.com>
2019-07-17 11:59:46 +0700

    first commit

---
+++ vouchsgchatbot/src/main/java/vouch/sg/vouchsgchatbot/models/Message.kt
@@ -0,0 +1,17 @@
+package vouch.sg.vouchsgchatbot.models
+
+class Message {
+    var bot: String? = null
+    var message: String? = null
+    var type: String? = null
+
+    constructor() {
+
+    }
+
+    constructor(bot: String, message: String, type:String) {
+        this.bot = bot
+        this.message = message
+        this.type = type
+    }
+}
